7e6d5cdcd442c1f515d9e77ef68a60fd2c06dc06
jhwanl <jjonghlee98@gmail.com>
2022-11-01 14:18:06 +0900

    commit lunch

--- src/main/resources/static/js/findid.js
+++ src/main/resources/static/js/findid.js
@@ -1,26 +1,26 @@
 const findIdEmail = document.getElementById("findId-email")
 const findIdBtn = document.getElementById("findId__button")
+const yourIdContainer = document.getElementById("yourIdContainer");
+const yourId = document.getElementById("yourId");
 const token = sessionStorage.getItem('jwt');
 const tokenHead = "Bearer " + token;
 
 findIdBtn.addEventListener("click", () => {
-  
-  const req = {
-    u_email: findIdEmail.value,
-  };
-  const arrReq = [];
-  arrReq.push(req);
 
-  console.log(arrReq);
-  console.log(JSON.stringify(arrReq));
-  fetch("/login/findMyId", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(arrReq),
-  })
-    .then((response) => response.json())
-    .then((findIdData) => {
-      //서버에서 u_id 보내줌
-    });
+    const req = {
+        u_email: findIdEmail.value,
+    };
+
+    fetch("/login/findMyId", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(req),
+    })
+        .then((response) => response.json())
+        .then((findIdData) => {
+            console.log(findIdData);
+            yourIdContainer.style.display = "block";
+            yourId.value = findIdData.u_id;
+        });
 
 })
--- src/main/resources/static/js/findpw.js
+++ src/main/resources/static/js/findpw.js
@@ -1,27 +1,56 @@
 const findPwId = document.getElementById("findPw-id");
 const findPwEmail = document.getElementById("findPw-email");
 const findPwBtn = document.getElementById("findPw__button");
-
+const findPwInfoEmail = document.getElementById("findPw-info__email");
+const yourPwContainer = document.getElementById("yourPwContainer");
+const yourNewPw = document.getElementById("yourNewPw");
 
 findPwBtn.addEventListener("click", () => {
+    if (yourPwContainer.style.display === '') {
 
-  const req = {
-    u_id: findPwId.value,
-    u_email: findPwEmail.value,
-  };
-  const arrReq = [];
-  arrReq.push(req);
+        const req = {
+            u_id: findPwId.value,
+            u_email: findPwEmail.value,
+        };
 
-  console.log(arrReq);
-  console.log(JSON.stringify(arrReq));
-  fetch("/login/findMyPw", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(arrReq),
-  })
-    .then((response) => response.json())
-    .then((findPwdata) => {
-      alert("비밀번호: " + findPwdata)
-    });
+        fetch("/login/checkMyInfo", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(req),
+        })
+            .then((response) => response.json())
+            .then((findPwData) => {
+                console.log(findPwData);
+                if (findPwData === 1) {
+                    yourPwContainer.style.display = "flex";
+                    findPwInfoEmail.style.display = "none";
+                } else {
+                    alert("아이디 혹은 이메일을 다시 확인하세요.");
+                }
+            });
 
+    } else {
+
+        const req = {
+            u_id: findPwId.value,
+            u_pw: yourNewPw.value,
+        };
+
+        fetch("/login/setNewPw", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(req),
+        })
+            .then((response) => response.json())
+            .then((findPwData) => {
+                console.log(findPwData);
+                if (findPwData === 1) {
+                    alert("변경되었습니다.");
+                    location.href = "/mainPage";
+                } else {
+                    alert("오류");
+                }
+            });
+
+    }
 })
